73384176f84f523ec3f887a9288558d407d7d0f1
pidougao <pidougao@qq.com>
2024-05-31 09:12:22 +0800

    feat: 接收者且无缓存区，发送者(sender)会一直阻塞，导致协程不能退出

--- go-with-test/gortimeout/gtimeout.go
+++ go-with-test/gortimeout/gtimeout.go
@@ -6,19 +6,21 @@ import (
 )
 
 func doSomething(ch chan bool) {
-	fmt.Printf("do something")
+	fmt.Printf("do something\n")
 	time.Sleep(time.Second)
 	ch <- true
 }
 
 func timeout(f func(chan bool)) error {
-	done := make(chan bool)
+	//接收者无缓存区，发送者(sender)会一直阻塞，导致协程不能退出
+	//done := make(chan bool)
+	done := make(chan bool, 1)
 	go f(done)
 	select {
 	case <-done:
 		fmt.Println("done")
 		return nil
-	case <-time.After(time.Millisecond * 500):
+	case <-time.After(time.Millisecond):
 		return fmt.Errorf("timeout")
 	}
 }
--- go-with-test/gortimeout/gtimeout_test.go
+++ go-with-test/gortimeout/gtimeout_test.go
@@ -2,11 +2,22 @@ package gortimeout
 
 import (
 	"github.com/stretchr/testify/assert"
+	"runtime"
 	"testing"
+	"time"
 )
 
 func TestTimeout(t *testing.T) {
 	err := timeout(doSomething)
 	assert.Error(t, err)
 	//assert.Equal(t, nil, err)
+
+	t.Run("1000", func(t *testing.T) {
+		t.Helper()
+		for i := 0; i < 1000; i++ {
+			timeout(doSomething)
+		}
+		time.Sleep(time.Second * 2)
+		t.Log(runtime.NumGoroutine())
+	})
 }
