40862802beea6baadd6c114dcc50c24da03a75cc
pidougao <pidougao@qq.com>
2024-06-02 22:32:43 +0800

    feat: 使用 select 尝试发送

--- go-with-test/gortimeout/gtimeout.go
+++ go-with-test/gortimeout/gtimeout.go
@@ -2,25 +2,46 @@ package gortimeout
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 func doSomething(ch chan bool) {
-	time.Sleep(time.Millisecond * 500)
+	time.Sleep(time.Millisecond * 10)
 	ch <- true
 }
 
+func doSomethingGood(ch chan bool) {
+	time.Sleep(time.Millisecond * 10)
+	//select 尝试向信道 done 发送信号，如果发送失败，则说明缺少接收者(receiver)，即超时了，那么直接退出即可
+	select {
+	case ch <- true:
+		//fmt.Println("数据发至通道..")
+	default:
+		//fmt.Println("通道数据等待读取中...")
+		return
+	}
+}
+
+// randInt 返回一个在 [min, max) 范围内的随机整数
+func randInt(min, max int) int {
+	r := rand.New(rand.NewSource(time.Now().UnixNano()))
+	return r.Intn(max-min) + min
+}
+
 func timeout(f func(chan bool)) error {
-	//接收者无缓存区，发送者(sender)会一直阻塞，导致协程不能退出
+	// 接收者无缓存区，发送者(doSomething)会一直阻塞，导致协程不能退出，doSomethingGood不存在这个问题（default直接返回了）
 	//done := make(chan bool)
 	//创建channel done 时，缓冲区设置为 1，即使没有接收方，发送方也不会发生阻塞
 	done := make(chan bool, 1)
 	go f(done)
+
 	select {
 	case <-done:
-		fmt.Println("done")
+		//fmt.Println("从通道读取到数据..")
 		return nil
-	case <-time.After(time.Millisecond):
+	case <-time.After(time.Millisecond * time.Duration(randInt(10, 20))):
+		//fmt.Println("从通道读数据超时..")
 		return fmt.Errorf("timeout")
 	}
 }
--- go-with-test/gortimeout/gtimeout_test.go
+++ go-with-test/gortimeout/gtimeout_test.go
@@ -8,23 +8,36 @@ import (
 )
 
 func TestTimeout(t *testing.T) {
-	t.Log(runtime.NumGoroutine())
-	err := timeout(doSomething)
-	assert.Error(t, err)
+	//t.Log(runtime.NumGoroutine())
+	_ = timeout(doSomething)
+	//assert.Error(t, err)
 	//assert.Equal
-	t.Log(runtime.NumGoroutine())
+	//t.Log(runtime.NumGoroutine())
 	//for i := 0; i < 1000; i++ {
 	//	timeout(doSomething)
 	//}
 	time.Sleep(time.Second * 1)
-	t.Log(runtime.NumGoroutine())
+	//t.Log(runtime.NumGoroutine())
 
-	t.Run("1000", func(t *testing.T) {
+	t.Run("test doSomething", func(t *testing.T) {
 		t.Helper()
-		for i := 0; i < 1000; i++ {
+		for i := 0; i < 100; i++ {
 			timeout(doSomething)
 		}
 		time.Sleep(time.Second * 2)
 		t.Log(runtime.NumGoroutine())
+		assert.Equal(t, 3, runtime.NumGoroutine())
+	})
+}
+
+func TestGoodTimeout(t *testing.T) {
+	t.Run("test doSomethingGood", func(t *testing.T) {
+		t.Helper()
+		for i := 0; i < 100; i++ {
+			timeout(doSomethingGood)
+		}
+		time.Sleep(time.Second * 2)
+		t.Log(runtime.NumGoroutine())
+		assert.Equal(t, 3, runtime.NumGoroutine())
 	})
 }
