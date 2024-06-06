aa84b9a15daef6ac187a67dc11a6fab6c75ea5ea
pidougao <pidougao@qq.com>
2024-06-06 08:23:54 +0800

    nums := [...]int{1, 2, 3, 4} 和 nums := []int{1, 2, 3, 4} 这两种写法在 Go 语言中有显著的区别。它们分别定义了数组和切片。

--- go-with-test/arrayslice/sum_test.go
+++ go-with-test/arrayslice/sum_test.go
@@ -76,3 +76,51 @@ func TestSquare(t *testing.T) {
 		t.Errorf("failed, a[1]=%d,a[2]=%d", a[1], a[2])
 	}
 }
+
+func printLenCap(nums []int) {
+	fmt.Printf("%v: len=%d, cap=%d\n", nums, cap(nums), len(nums))
+}
+
+// === RUN   TestSliceLenAndCap
+// [1]: len=1, cap=1
+// [1 2]: len=2, cap=2
+// [1 2 3]: len=4, cap=3
+// [1 2 3 4]: len=4, cap=4
+// [1 2 3 4 5]: len=8, cap=5
+// --- PASS: TestSliceLenAndCap (0.00s)
+// 往切片中不断地增加新的元素。如果超过了当前切片的容量，就需要分配新的内存，并将当前切片所有的元素拷贝到新的内存块上
+// 容量在比较小的时候，一般是以 2 的倍数扩大的，例如 2 4 8 16 …，当达到 2048 时，会采取新的策略
+func TestSliceLenAndCap(t *testing.T) {
+	nums := []int{1}
+	printLenCap(nums)
+	nums = append(nums, 2)
+	printLenCap(nums)
+	nums = append(nums, 3)
+	printLenCap(nums)
+	nums = append(nums, 4)
+	printLenCap(nums)
+	nums = append(nums, 5)
+	printLenCap(nums)
+}
+
+// https://geektutu.com/post/hpg-slice.html
+// === RUN   TestSliceAppend
+// [1 2 3 4 5]: len=8, cap=5
+// [3 4]: len=6, cap=2
+// [1 2 3 4 50]: len=8, cap=5
+// [3 4 50 60]: len=6, cap=4
+// --- PASS: TestSliceAppend (0.00s)
+// PASS
+func TestSliceAppend(t *testing.T) {
+	nums := make([]int, 0, 8)
+	nums = append(nums, 1, 2, 3, 4, 5)
+	nums2 := nums[2:4]
+	printLenCap(nums)  // len: 5, cap: 8 [1 2 3 4 5]
+	printLenCap(nums2) // len: 2, cap: 6 [3 4]
+
+	// nums2 增加 2 个元素 50 和 60 后，将底层数组下标 [4] 的值改为了 50，下标[5] 的值置为 60
+	// nums2[2]=50 会覆盖 nums[4]=5
+	nums2 = append(nums2, 50, 60)
+	printLenCap(nums)  // len: 5, cap: 8 [1 2 3 4 50]
+	printLenCap(nums2) // len: 4, cap: 6 [3 4 50 60]
+}
