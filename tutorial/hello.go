ad37040e3cdf0cd73f1abbf5e7a84ac1e9d71e46
pidougao <pidougao@qq.com>
2024-06-26 05:47:07 +0800

    feat: 空过滤条件处理

--- tutorial/hello.go
+++ tutorial/hello.go
@@ -1,7 +1,13 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
 	fmt.Println("hello world")
+	v1 := []string{}
+	fmt.Println(strings.Join(v1, ","))
+	fmt.Println("aaa")
 }
